911838855aba9a79f30efe08da06dd202f02274b
Weibo Scrapper <weiboscrapper@gmail.com>
2018-08-02 11:16:19 +0800

    updated entities.

--- src/main/java/com/practice/poll/entity/PollSubject.java
+++ src/main/java/com/practice/poll/entity/PollSubject.java
@@ -1,14 +1,16 @@
 package com.practice.poll.entity;
 
 import java.sql.*;
+import java.util.List;
 import javax.persistence.*;
 
 import lombok.Data;
 
 @Data
 @Entity(name = "com.practice.entity.PollSubject")
-@Table(name = "poll_subject",
-        indexes = { @Index(name = "idx_poll_subject_user_id", columnList = "user_id ASC")})
+@Table(
+        name = "poll_subject",
+        indexes = {@Index(name = "idx_poll_subject_user_id", columnList = "user_id ASC")})
 public class PollSubject {
 
     @Id
@@ -16,7 +18,9 @@ public class PollSubject {
     @Column(name = "\"id\"", nullable = false)
     private Long id;
 
-    @ManyToOne(cascade = {CascadeType.DETACH})
+    @ManyToOne(
+            cascade = CascadeType.ALL,
+            fetch=FetchType.LAZY)
     @JoinColumn(
             foreignKey = @ForeignKey(name = "fk_poll_subject_user_id"),
             name = "user_id",
@@ -24,8 +28,16 @@ public class PollSubject {
             nullable = false)
     private User user;
 
+    @OneToMany(
+            cascade = CascadeType.ALL,
+            orphanRemoval = true,
+            mappedBy = "pollSubject",
+            fetch=FetchType.LAZY)
+    private List<SubItem> subItemList;
+
     @Column(name = "\"subject\"", nullable = false, length = 128)
     private String subject;
+
     @Column(name = "\"expiration_time\"", nullable = false)
     private Timestamp expirationTime;
 }
